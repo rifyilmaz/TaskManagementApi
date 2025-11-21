0ff0641aea9ebff6b61869f8543af762159e9ab7
rifyilmaz <rifyilmaz@gmail.com>
2025-11-21 21:27:17 +0300

    Model Item Bitti

---
+++ Models/NewsItem.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskManagementApi.Models
+{
+    public class NewsItem
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; } = "";
+    }
+}
